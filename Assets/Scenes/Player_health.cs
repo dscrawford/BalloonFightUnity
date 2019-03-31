1a2870b9e58a5af68fcbb9ea16ec18ac18e65e78
AriaC <kac150030@utdallas.edu>
2019-03-30 21:01:49 -0500

    added assets for main menu and basic AI

---
+++ Assets/Scenes/Player_health.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player_health : MonoBehaviour
+{
+    public int health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.position.y < -6)
+        {
+            Debug.Log("Player deaded");
+            Die();
+        }
+        
+
+    }
+
+    void Die ()
+    {
+        SceneManager.LoadScene("Prototype_1");
+    }
+}
