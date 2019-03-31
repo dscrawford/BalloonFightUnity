4c2718c14643ce6a9e4aa6cfbac5854f70700b47
Daniel Crawford <daniel.sc.crawford@gmail.com>
2019-03-30 21:45:37 -0500

    Fresh start

---
+++ Assets/AriasStuff/Player_Move2.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Move2 : MonoBehaviour
+{
+    public int playerSpeed = 10;
+    private bool facingRight = false;
+    public int playerJumpPower = 1250;
+    private float moveX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        //CONTROLS
+        moveX = Input.GetAxis("Horizontal");
+        if(Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+
+        //ANIME
+
+
+        //PLAYER DIRECTION
+        if(moveX < 0.0f && !facingRight)
+        {
+            FlipPlayer();
+        }
+        else if (moveX > 0.0f && facingRight)
+        {
+            FlipPlayer();
+        }
+        //PHYSICS
+
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+
+
+    void Jump()
+    {
+        //JUMPING CODE
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
+    }
+
+    void FlipPlayer()
+    {
+        facingRight = !facingRight;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+}
---
+++ Assets/Scripts/Player_Move.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Move : MonoBehaviour
+{
+    public int playerSpeed = 1;
+    public bool facingRight = false;
+    public int playerJumpPower = 20;
+    public float maxYVelocity = 5;
+
+    private float moveX, moveY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        moveY = GetComponent<Rigidbody2D>().velocity.y;
+        //Controls
+        if (Input.GetButtonDown("Jump")) { 
+            Jump();
+        }
+        //Animation
+
+        //Player Direction
+        if (moveX < 0.0f && !facingRight)
+        {
+            FlipPlayer();
+        }
+        else if (moveX > 0.0f && facingRight)
+        {
+            FlipPlayer();
+        }
+
+        //Physics
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, Mathf.Clamp(GetComponent<Rigidbody2D>().velocity.y,-maxYVelocity, maxYVelocity));
+    }
+
+    void Jump()
+    {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
+    }
+
+    float GetYMovementSpeed()
+    {
+        if (moveY > maxYVelocity)
+        {
+            return maxYVelocity;
+        }
+        return moveY;
+    }
+
+    void FlipPlayer()
+    {
+        facingRight = !facingRight;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+}
