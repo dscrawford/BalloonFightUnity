aa0b168c039dc823e1983074a50b4fd78052ad63
William12Ryder <48457583+William12Ryder@users.noreply.github.com>
2019-04-20 18:37:31 -0500

    Implemented some tying of the code together with the animation controller for the player
    Still need to put together connections to the enemy animation controller

--- Assets/Scripts/Player_Move.cs
+++ Assets/Scripts/Player_Move.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class Player_Move : MonoBehaviour
 {
+    public Animator anim;
+
     public int playerSpeed = 10;
     public int playerJumpPower = 20;
     public float maxYVelocity = 5;
 
-     private float moveX, moveY;
+    private float moveX, moveY;
     private bool facingRight = false;
 
     // Start is called before the first frame update
     void Start()
-    {}
+    {
+        anim = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -26,6 +30,17 @@ public class Player_Move : MonoBehaviour
         return moveX;
     }
 
+    void OnCollisionEnter()
+    {
+        //anim.SetBool("Ground", true);
+    }
+
+    void OnCollisionExit()
+    {
+        //anim.SetBool("Ground", false);
+    }
+
+
     void PlayerMove()
     {
         moveX = Input.GetAxis("Horizontal");
@@ -35,6 +50,24 @@ public class Player_Move : MonoBehaviour
             Jump();
         }
         //Animation
+        if(Input.GetButtonDown("Jump"))
+        {
+            anim.SetBool("Jump", true);
+        }
+        else
+            anim.SetBool("Jump", false);
+        if (moveY != 0.0f) //NOT on ground Needs correction due to breif frame shifting from uplift to falling
+        {
+            anim.SetBool("Ground", false);
+        }
+        else
+            anim.SetBool("Ground", true);
+        if (moveX != 0.0f)
+        {
+            anim.SetBool("Move", true);
+        }
+        else
+            anim.SetBool("Move", false);
 
         //Player Direction
         if (moveX < 0.0f && facingRight)
